30c9f16ab52b16beb3cf4cd18942b655e3dd2c96
tyleraland <tyleraland@gmail.com>
2013-07-24 11:38:38 -0700

    Better eat regex; TODO: manually assign db_id to foods, use sqlite

--- logger.py
+++ logger.py
@@ -5,5 +5,4 @@ import specials
 # Calls timestamp.py to sort
 jsfile = '/Users/tal/Code/tweets/data/js/tweets/2013_06.js'
 tweets = timestamps.sort_time(jsfile)
-#print(tweets)
-#specials.process(tweets)
+specials.process(tweets)
--- specials.py
+++ specials.py
@@ -2,24 +2,30 @@ import re
 import csv
 
 wo = re.compile('([a-zA-Z]*.?[a-zA-Z]+)@? ([\d.\s]+)+')
-#eat = re.compile('([a-z][\.a-z]*\.[a-z]+)\.([0-9]*)\.([a-z]*)')
-eat = re.compile('([a-z][\.a-z]*\.[a-z]+)\.([0-9]*)([a-z]*)')
+eat = re.compile('([.a-z]*[a-z]+)(?:\.)?([0-9]*)([a-z]*)')
 wolog = csv.writer(open('DUMMYCSV.csv','w'), quoting=csv.QUOTE_ALL)
+# wolog header
+# times: "year monthnumber daynumber hour minute"
+# wo_id is shared between sets from the same session
+# lifts: "weight1:rep1-rep2-...-repN;weight2:rep1-rep2-..."
 wolog.writerow(["start_time","end_time",
-                "wo_id","exercise","lifts"]) # Header
+                "wo_id","exercise","lifts"])
+
+new_foods = []
+food_table = {} # Load from file
 
 def workout(tweet,wo_id):
     text = tweet[2][3:]
     exers = re.findall(wo,text)
     for exer in exers:
         row = []
-        row.append(" ".join([str(field) for field in tweet[0]])) # Start time
-        row.append(" ".join([str(field) for field in tweet[1]])) # Stop time
+        row.append(" ".join([str(field) for field in tweet[0].timetuple()[:-4]])) # Start
+        row.append(" ".join([str(field) for field in tweet[1].timetuple()[:-4]])) # Stop time
         row.append(wo_id)
-        lift_name = exer[0]
+        lift_name = exer[0]  # TODO: Use lift_name as key into lift_alias table, use standardized name
         wgroups = exer[1].split() #e.g. ['100.9.8', '120.5.5']
-        lifts = '' #later: weight:rep1-rep2-rep3;weight:rep1-rep2"
-        for wgroup in wgroups: #e.g. '100.9.8'
+        lifts = '' # See wolog header comment above
+        for wgroup in wgroups: #e.g. wogroup = '100.9.8'
             nums = wgroup.split('.')
             if len(lifts) > 0:
                 lifts += ';'
@@ -35,9 +41,11 @@ def consume(tweet):
     if match[0][1]:
         quantity = match[0][1]
     else:
-        quantity = -1
+        quantity = -1 # Force breakage.  Each db_id should have default value
     if match[0][2]:
         unit = match[0][2]
+    if not name in food_table:
+        new_foods.append(name)
 
 def process(tweets):
     wo_id = 1
@@ -49,3 +57,7 @@ def process(tweets):
             wo_id += 1
         if clue == 'eat': # eat
             consume(tweet)
+    # Ask user about new_foods
+    for food in new_foods:
+        print(food)
+    # Save food_table to file
